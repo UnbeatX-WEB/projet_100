c81eb6038463e6f6b1f38e7a5f9e1c3b59b10f19
UnbeatX <UnbeatX@proton.me>
2026-09-15 18:57:41 +0400

    Add files via upload

---
+++ Projet_11/Projet_11.bat
@@ -0,0 +1,18 @@
+@echo off
+title Terminal demo
+color a
+
+echo ================================
+echo         Terminal Demo
+echo ================================
+echo.
+echo connection au serveur...
+timeout /t 1 >nul
+echo Verification...
+timeout /t 1 >nul
+echo [OK] Systeme securise.
+echo.
+echo Analyse Terminee
+echo aucun fichier modifié
+echo.
+pause

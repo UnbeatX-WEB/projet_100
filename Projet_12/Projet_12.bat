9f353e6fa4fa5d8898b7333b919a74236e930f90
UnbeatX <UnbeatX@proton.me>
2026-09-16 18:31:45 +0400

    Add files via upload

---
+++ Projet_12/Projet_12.bat
@@ -0,0 +1,25 @@
+@echo off
+title System Terminal
+color 0a
+
+cls
+echo.
+echo ================================
+echo          SYSTEM TERMINAL
+echo ================================
+echo.
+echo [INFO] Démarrage du systeme...
+timeout /t 1 >nul
+echo [INFO] Verification des modules...
+timeout /t 1 >nul
+echo [OK] Tous les modules sont acitfs.
+echo.
+echo [scan] Analyse en cours...
+timeout /t 2 >nul
+echo [OK] Aucun probleme détecte.
+echo.
+echo ================================
+echo           SYSTEM READY
+echo ================================
+echo.
+pause
